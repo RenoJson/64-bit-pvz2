fb352227fc7f635ca4101f75d9b2d1e929826d18
RenoJson <cresht704@gmail.com>
2026-05-19 15:22:42 +0700

    little fix for bassist

--- PvZ2/AddProjectileType.h
+++ PvZ2/AddProjectileType.h
@@ -6,6 +6,6 @@
 inline Projectile* AddProjectile(Sexy::RtWeakPtr<ProjectilePropertySheet>* type, BoardEntity* owner, float mX, float mY, float mZ) {
 	Board* board = Board::GetBoard();
 	typedef Projectile* (*addProjectileByType)(Board*, Sexy::RtWeakPtr<ProjectilePropertySheet>*, BoardEntity*, float, float, float);
-	addProjectileByType funAddProjectileByType = (addProjectileByType)getActualOffset(0xAAE9F0);
+	addProjectileByType funAddProjectileByType = (addProjectileByType)getActualOffset(0xAAE5B4);
 	return funAddProjectileByType(board, type, owner, mX, mY, mZ);
 }
--- PvZ2/ZombieEightiesBass.cpp
+++ PvZ2/ZombieEightiesBass.cpp
@@ -11,12 +11,16 @@
 
 void* ZombieEightiesBass::vftable = nullptr;
 Sexy::RtClass* ZombieEightiesBass::s_rtClass = nullptr;;
-typedef void (*zombieEnterState)(ZombieEightiesBass*, int, int);
+typedef void (*zombieEnterState)(Zombie*, int, int);
 typedef int (*playAnimWithCallback)(ZombieAnimRig*, const SexyString&, int, ZombieEvent& event);
 typedef ZombieEvent* (*ConstructEvent)(ZombieEvent*, RtWeakPtr<Zombie>& owner, const SexyString& eventName);
 typedef Zombie* (*zombieFlippedAnim)(Zombie*, int);
 typedef bool (*isAnimDone)(ZombieAnimRig*, int);
+
+typedef void (*setSpeedScale)(ZombieEightiesBass*, float);
 typedef void (*GetEntitiesInRectFunc)(std::vector<BoardEntity*>*, int, Rect*);
+
+typedef Zombie* (*updatePos)(Zombie*, SexyVector3*);
 DECLARE_DELEGATES_SETUP(ZombieEightiesBass)
 
 static Sexy::DelegateBase riftingCompletedDelegate;
@@ -63,31 +67,42 @@ void BassActionFrame(ZombieEightiesBass* zombie, int64_t unk1, SexyString* actio
 {
     if (*actionName == "kill_plant")
     {
-        typedef PlantGroup* (*getTarg)(ZombieEightiesBass*);
-        getTarg getTarget = (getTarg)getActualOffset(0xC41910);
-        PlantGroup* target = getTarget(zombie);
-        if (target != nullptr && target->IsType(PlantGroup::StaticGetType())) {
+        int gX = static_cast<int>((zombie->m_position.x - 200.0f) / 64.0f);
+        int gY = static_cast<int>((zombie->m_position.y - 160.0f) / 76.0f);
 
-            DamageInfo dmg;
-            dmg.m_attacker = zombie;
+        if (gY > 4) gY = 4;
+        if (gY < 0) gY = 0;
 
-            void** vtable = *(void***)target;
-            typedef void (*VirtualTakeDamageFunc)(PlantGroup*, DamageInfo*);
-            VirtualTakeDamageFunc takeDmg = (VirtualTakeDamageFunc)vtable[36];
-            takeDmg(target, &dmg);
-        }
-        else {
-            return;
+        Rect scanRect;
+        scanRect.mX = gX - 1;
+        scanRect.mY = gY;
+        scanRect.mWidth = 1;
+        scanRect.mHeight = 1;
+
+        std::vector<BoardEntity*> entityList;
+
+        GetEntitiesInRectFunc getEntitiesRect = (GetEntitiesInRectFunc)getActualOffset(0x86F180);
+        getEntitiesRect(&entityList, 63, &scanRect);
+
+        for (BoardEntity* ptr : entityList) {
+            if (ptr == nullptr) continue;
+
+            if (ptr->IsType(PlantGroup::StaticGetType()))
+            {
+                DamageInfo dmg;
+                dmg.m_attacker = zombie;
+
+                void** vtable = *(void***)ptr;
+                typedef void (*VirtualTakeDamageFunc)(PlantGroup*, DamageInfo*);
+                VirtualTakeDamageFunc takeDmg = (VirtualTakeDamageFunc)vtable[36];
+
+                takeDmg((PlantGroup*)ptr, &dmg);
+            }
         }
     }
 }
 
 void ZombieEightiesBass::GuitarAttackOnEnter(ZombieEightiesBass* zombie)
-{
-    RegisterEventAfterAnim(zombie, "attack", "onWaitingToRiftEnd");
-}
-
-void ZombieEightiesBass::GuitarAttackOnLoop(ZombieEightiesBass* zombie)
 {
     int gX = static_cast<int>((zombie->m_position.x - 200.0f) / 64.0f);
     int gY = static_cast<int>((zombie->m_position.y - 160.0f) / 76.0f);
@@ -113,24 +128,29 @@ void ZombieEightiesBass::GuitarAttackOnLoop(ZombieEightiesBass* zombie)
             break;
         }
     }
-
     if (targetSpeaker != nullptr) {
         auto* props = reinterpret_cast<ZombieEightiesBassProps*>(zombie->m_propertySheet.Get());
         Projectile* shockwave = AddProjectile(&props->ShockWaveProjectile,
-            zombie,
-            targetSpeaker->m_position.x - props->ShockWaveSpawnOffset.x,
-            targetSpeaker->m_position.y - props->ShockWaveSpawnOffset.y,
-            0);
-		shockwave->m_teamFlags = zombie->m_teamFlags;
+                                                zombie,
+                                                targetSpeaker->m_position.x - props->ShockWaveSpawnOffset.x,
+                                                targetSpeaker->m_position.y - props->ShockWaveSpawnOffset.y,
+                                                0);
+        shockwave->m_teamFlags = zombie->m_teamFlags;
+        ((zombieEnterState)getActualOffset(0xC3D428))(zombie, 17, 0);
     }
     else {
         ((zombieEnterState)getActualOffset(0xC3D428))(zombie, 18, 0);
     }
 }
 
+void ZombieEightiesBass::GuitarAttackOnLoop(ZombieEightiesBass* zombie)
+{
+   
+}
+
 void ZombieEightiesBass::GuitarAttackOnExit(ZombieEightiesBass* zombie)
 {
-    ((zombieEnterState)getActualOffset(0xC3D428))(zombie, 17, 0);
+    
 }
 
 void ZombieEightiesBass::GuitarIdleOnEnter(ZombieEightiesBass* zombie)
@@ -177,16 +197,16 @@ void ZombieEightiesBass::GuitarIdleOnLoop(ZombieEightiesBass* zombie)
     }
 
     if (targetSpeaker != nullptr) {
-        auto* props = reinterpret_cast<ZombieEightiesBassProps*>(zombie->m_propertySheet.Get());
-        if (zombie->m_isJamming && zombie->m_elapsedTimeInState >= props->ShockWaveSpawnIntervalWhileJamming) {
-            ((zombieEnterState)getActualOffset(0xC3D428))(zombie, 16, 0);
-		}
-        else if (!zombie->m_isJamming && zombie->m_elapsedTimeInState >= props->ShockWaveSpawnInterval) {
-            ((zombieEnterState)getActualOffset(0xC3D428))(zombie, 16, 0);
-		}
-        else {
-            if (animDone)
-            {
+        if (animDone)
+        {
+            auto* props = reinterpret_cast<ZombieEightiesBassProps*>(zombie->m_propertySheet.Get());
+            if (zombie->m_isJamming && zombie->m_elapsedTimeInState >= props->ShockWaveSpawnIntervalWhileJamming) {
+                ((zombieEnterState)getActualOffset(0xC3D428))(zombie, 16, 0);
+		    }
+            else if (!zombie->m_isJamming && zombie->m_elapsedTimeInState >= props->ShockWaveSpawnInterval) {
+                ((zombieEnterState)getActualOffset(0xC3D428))(zombie, 16, 0);
+		    }
+            else {
                 RtWeakPtr<Zombie> zombiePtr;
                 zombiePtr.FromOther((RtWeakPtr<Zombie>*) & zombie->m_thisPtr);
 
@@ -245,39 +265,51 @@ void RiftingCompletedCallback(Zombie* zombie) {
 }
 
 void DebutCompletedCallback(Zombie* zombie) {
-	ZombieEightiesBass* bassZombie = static_cast<ZombieEightiesBass*>(zombie);
-	if (bassZombie) {
-        typedef PlantGroup* (*getTarg)(ZombieEightiesBass*);
-        getTarg getTarget = (getTarg)getActualOffset(0xC41910);
-        PlantGroup* target = getTarget(bassZombie);
+    ZombieEightiesBass* bassZombie = static_cast<ZombieEightiesBass*>(zombie);
+    if (bassZombie) {
+        int gX = static_cast<int>((bassZombie->m_position.x - 200.0f) / 64.0f);
+        int gY = static_cast<int>((bassZombie->m_position.y - 160.0f) / 76.0f);
+
+        if (gY > 4) gY = 4;
+        if (gY < 0) gY = 0;
+
+        Rect scanRect;
+        scanRect.mX = gX - 1;
+        scanRect.mY = gY;
+        scanRect.mWidth = 1;
+        scanRect.mHeight = 1;
+
+        std::vector<BoardEntity*> entityList;
+
+        GetEntitiesInRectFunc getEntitiesRect = (GetEntitiesInRectFunc)getActualOffset(0x86F180);
+        getEntitiesRect(&entityList, 63, &scanRect);
+
+        for (BoardEntity* ptr : entityList) {
+            if (ptr == nullptr) continue;
+
+            if (ptr->IsType(PlantGroup::StaticGetType()))
+            {
+                DamageInfo dmg;
+                dmg.m_attacker = bassZombie;
+
+                void** vtable = *(void***)ptr;
+                typedef void (*VirtualTakeDamageFunc)(PlantGroup*, DamageInfo*);
+                VirtualTakeDamageFunc takeDmg = (VirtualTakeDamageFunc)vtable[36];
+
+                takeDmg((PlantGroup*)ptr, &dmg);
+            }
+        }
         float rawPosX;
         float rawPosY;
         int spawnPosX;
         int spawnPosY;
 
-        if (target != nullptr && target->IsType(PlantGroup::StaticGetType())) {
-            rawPosX = target->m_position.x;
-            rawPosY = target->m_position.y;
+        rawPosX = bassZombie->m_position.x;
+        rawPosY = bassZombie->m_position.y;
 
-            spawnPosX = (int)(((rawPosX - 232.0f) / 64.0f));
-            spawnPosY = (int)(((rawPosY - 160.0f) / 76.0f));
+        spawnPosX = (int)(((rawPosX - 200.0f) / 64.0f)) - 1;
+        spawnPosY = (int)(((rawPosY - 160.0f) / 76.0f));
 
-
-            DamageInfo dmg;
-            dmg.m_attacker = bassZombie;
-
-            void** vtable = *(void***)target;
-            typedef void (*VirtualTakeDamageFunc)(PlantGroup*, DamageInfo*);
-            VirtualTakeDamageFunc takeDmg = (VirtualTakeDamageFunc)vtable[36];
-            takeDmg(target, &dmg);
-        }
-        else {
-            rawPosX = bassZombie->m_position.x;
-            rawPosY = bassZombie->m_position.y;
-
-            spawnPosX = (int)(((rawPosX - 232.0f) / 64.0f) + 0.5f) - 1;
-            spawnPosY = (int)(((rawPosY - 160.0f) / 76.0f));
-        }
 
         Board* board = Board::GetBoard();
 
@@ -286,25 +318,36 @@ void DebutCompletedCallback(Zombie* zombie) {
         if (spawnPosY < 0) spawnPosY = 0;
         if (spawnPosY > 4) spawnPosY = 4;
 
-        typedef GridItemSurfboard* (*funcAA230C)(Board*, SexyString, int, int);
+        typedef GridItemSpeaker* (*funcAA230C)(Board*, SexyString, int, int);
         funcAA230C func_AA230C = (funcAA230C)getActualOffset(0xAA230C);
-        GridItemSurfboard* gridItem = func_AA230C(board, "speaker", spawnPosX, spawnPosY);
-
-		bassZombie->m_isInGrandDebut = false;
-		((zombieEnterState)getActualOffset(0xC3D428))(bassZombie, 17, 0);
-	}
+        GridItemSpeaker* gridItem = func_AA230C(board, "speaker", spawnPosX, spawnPosY);
+        gridItem->m_speakerState = 2;
+        bassZombie->m_isInGrandDebut = false;
+        ((zombieEnterState)getActualOffset(0xC3D428))(bassZombie, 17, 0);
+    }
 }
 void BreakingCompletedCallback(Zombie* zombie) {
 	ZombieEightiesBass* bassZombie = static_cast<ZombieEightiesBass*>(zombie);
 	if (bassZombie) {
         auto animRig = reinterpret_cast<ZombieAnimRig_EightiesBass*>(bassZombie->m_animRig.Get());
         animRig->m_hasGuitar = false;
+        bassZombie->m_dpsScale = 2.0f;
+        ((setSpeedScale)getActualOffset(0xC484C0))(bassZombie, 2.0f);
 		((zombieEnterState)getActualOffset(0xC3D428))(bassZombie, 1, 0);
 	}
 }
+Zombie* updateParachutePos(float mX, float mY, float mZ, void* a4, Zombie* zombie) {
+	SexyVector3 offset = { zombie->m_position.x, zombie->m_position.y, 0 };
+    if(zombie->IsType(ZombieEightiesBass::StaticGetType())) {
+        offset.x += 10.0f;
+        offset.y += 10.0f;
+	}
+    ((zombieEnterState)getActualOffset(0xC3D428))(zombie, 19, 0);
+    return ((updatePos)getActualOffset(0x628278))(zombie, &offset);
+}
 void ZombieEightiesBass::ModInit() {
-    LOGI("ZombieMiner mod init");
-
+    LOGI("ZombieBass mod init");
+    PVZ2HookFunction(0x110BD94, (void*)updateParachutePos, nullptr);
     vftable = CreateChildVFTable(204 + 15, getActualOffset(0x241D430), 204);
     PatchVFTable(vftable, (void*)ZombieEightiesBass::StaticGetType, 0);
 
@@ -331,7 +374,7 @@ void ZombieEightiesBass::ModInit() {
     PatchVFTable(vftable, (void*)ZombieEightiesBass::GrandDebutOnExit, 215);
 
     ZombieEightiesBass::StaticGetType();
-    LOGI("ZombieMiner finish init");
+    LOGI("ZombieBass finish init");
 }
 
 void ZombieEightiesBass::buildEventCallbacks(Reflection::CRefManualSymbolBuilder* builder, Reflection::RClass* rtClass)
